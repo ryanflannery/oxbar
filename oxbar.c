ff24d11ea16d4d8911af123a064cfae6928c6eb2
ryan <ryan.flannery@gmail.com>
2018-09-17 23:16:51 -0400

    Switch from buggy signal timer handling to threads
    I .. i can't believe i typed this all out and it compiled and ran the
    first time. It seems ok after about 2 minutes of runtime and multiple
    restarts. I need to quit now for the night while i'm ahead.
    
    This removes the old super buggy (but quick!) approach to timing where i
    would call non-reentrant code (stats_update() and ui_draw()) in the
    signal handler used by the 1-second timer. Bad, i know, but it was cheap
    and quick when i entroduced the blocking xcb event loop to handle expose
    events, and it only caused crashes/hangs like, once every few hours.
    pffft.
    
    Anyway this fixes that by introducing 3 threads and moving logic into
    each appropriately:
    1. gui thread: just blocks on events and redraws.
    2. stats update thread: sleep for 1 second then updates stats and
       redraws.
    3. signal handler: registers signal handlers and will listen for them
       (the other 2 threads ignore all signals) and then will handle a kill.
    
    I need to guard the draw with a mutex, and ideally enable/disable
    pthread cancel state in the gui and stats threads better. Those are
    coming next.

--- oxbar.c
+++ oxbar.c
@@ -4,129 +4,129 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <sys/types.h>
 
 #include "gui.h"
 #include "settings.h"
 #include "stats/stats.h"
 
-oxbarui_t *g_ui;
+oxbarui_t *gui;
+pthread_t  pthread_gui;
+pthread_t  pthread_stats_updater;
+pthread_t  pthread_sig_handler;
 
-void signal_handler(int sig);
-void setup_timer();
-
-int
-main(int argc, char *argv[])
+void*
+thread_gui()
 {
-   settings_t settings;
+   sigset_t set;
+   sigfillset(&set);
+   pthread_sigmask(SIG_SETMASK, &set, NULL);  /* TODO check */
 
-   settings_load_defaults(&settings);
-   settings_parse_cmdline(&settings, argc, argv);
-
-   g_ui = ui_init(&settings);
-   stats_init();
-
-   /* NOTE: we are now ready to update stats and draw.... */
-   stats_update();   /* first stats collection */
-   ui_draw(g_ui);     /* initializes first-time widget properties */
-
-   signal(SIGHUP,  signal_handler);
-   signal(SIGINT,  signal_handler);
-   signal(SIGQUIT, signal_handler);
-   signal(SIGTERM, signal_handler);
-
-   /* this starts updating stats AND drawing oxbar every 1 second */
-   setup_timer();
-
-   /*
-    * this starts the xcb loop waiting for expose events to redraw, which
-    * occurs anytime the window is covered and then re-exposed
-    */
    xcb_generic_event_t *xevent;
-   while ((xevent = xcb_wait_for_event(g_ui->xinfo->xcon))) {
+   while ((xevent = xcb_wait_for_event(gui->xinfo->xcon))) {
       /* TODO WTF is "& ~0x80?" needed for in this xcb_event_t check?
        * This is straight from xcb documentation. I have no idea why it's
        * needed and haven't figured out yet.
        */
       switch (xevent->response_type & ~0x80) {
-         case XCB_EXPOSE:
-            ui_draw(g_ui);
-            break;
-         default:
-            /* TODO Identify what the other X events are we recieve */
-            break;
+      case XCB_EXPOSE:
+         ui_draw(gui);
+         break;
+      default:
+         break;
       }
    }
-
-   stats_close();
-   ui_free(g_ui);
-
-   return 0;
+   return NULL;
 }
 
-/* TODO Fix XCB event handling and signal handling
- * I have some issues below - namely in a signal handler I'm calling non-
- * reentrant safe code (stats_update() and ui_draw()).
- * That's horrible...and I know it.
- *
- * I need to rectify using xcb_wait_for_event(..) for my main X loop (a
- * blocking API) with my use of setitimer(2) for the 1-second updates
- * -or-
- * Go full pthread?
- * -or-
- * sem_poll(2) below?
- * -or-
- *  Something else...?
- */
+void*
+thread_stats_updater()
+{
+   sigset_t set;
+   sigfillset(&set);
+   pthread_sigmask(SIG_SETMASK, &set, NULL);  /* TODO check */
+
+   while (1) {
+      usleep(1000000);  /* 1 second */
+      stats_update();
+      ui_draw(gui);
+   }
+   return NULL;
+}
+
+volatile sig_atomic_t SIG_QUIT = 0;
+
 void
 signal_handler(int sig)
 {
    switch (sig) {
-      /* TODO On SIGHUP, reload config instead of quiting */
-      case SIGHUP:
-      case SIGINT:
-      case SIGQUIT:
-      case SIGTERM:
-         /* TODO Bad signal handler! Bad! These aren't reentrant!
-          * See todo above this function.
-          */
-         ui_free(g_ui);
-         stats_close();
-         exit(1);
-         break;
-      case SIGALRM:
-         /* TODO Bad signal handler! Bad! These aren't reentrant!
-          * See todo above this function.
-          */
-         stats_update();
-         ui_draw(g_ui);
-         break;
+   case SIGHUP:
+   case SIGINT:
+   case SIGTERM:
+   case SIGQUIT:
+      SIG_QUIT = 1;
+      break;
    }
 }
 
-void
-setup_timer()
+void*
+thread_sig_handler()
 {
+   sigset_t set;
+   sigemptyset(&set);
+   sigaddset(&set, SIGHUP);
+   sigaddset(&set, SIGINT);
+   sigaddset(&set, SIGTERM);
+   sigaddset(&set, SIGQUIT);
+   pthread_sigmask(SIG_SETMASK, &set, NULL);  /* TODO check */
+
    struct sigaction sig_act;
-   struct itimerval timer;
-
-   /* create timer signal handler */
-   if (0 > sigemptyset(&sig_act.sa_mask))
-      err(1, "%s: sigemptyset failed", __FUNCTION__);
-
    sig_act.sa_flags = 0;
    sig_act.sa_handler = signal_handler;
-   if (0 > sigaction(SIGALRM, &sig_act, NULL))
+   if (sigaction(SIGHUP,  &sig_act, NULL)
+    || sigaction(SIGINT,  &sig_act, NULL)
+    || sigaction(SIGTERM, &sig_act, NULL)
+    || sigaction(SIGQUIT, &sig_act, NULL))
       err(1, "%s: sigaction failed", __FUNCTION__);
 
-   /* setup timer details */
-   timer.it_value.tv_sec = 1;       /* wait 1 second for first timer fire */
-   timer.it_value.tv_usec = 0;
-   timer.it_interval.tv_sec = 1;    /* wait 1 second for subsequent fires */
-   timer.it_interval.tv_usec = 0;
-
-   /* register timer */
-   if (0 > setitimer(ITIMER_REAL, &timer, NULL))
-      err(1, "%s: setitimer failed", __FUNCTION__);
+   while (1) {
+      usleep(100000);  /* 1/10 second */
+      if (SIG_QUIT) {
+         pthread_cancel(pthread_gui);
+         pthread_cancel(pthread_stats_updater);
+         pthread_cancel(pthread_sig_handler);   /* ...it's painless */
+      }
+   }
+   return NULL;
 }
 
+int
+main(int argc, char *argv[])
+{
+   settings_t settings;
+   settings_load_defaults(&settings);
+   settings_parse_cmdline(&settings, argc, argv);
+
+   gui = ui_init(&settings);
+   stats_init();
+
+   /* we are now ready to update stats and draw / do first of each */
+   stats_update();
+   ui_draw(gui);
+
+   /* TODO checks */
+   pthread_create(&pthread_gui, NULL, thread_gui, NULL);
+   pthread_create(&pthread_stats_updater, NULL, thread_stats_updater, NULL);
+   pthread_create(&pthread_sig_handler, NULL, thread_sig_handler, NULL);
+
+   /* TODO checks */
+   pthread_join(pthread_gui, NULL);
+   pthread_join(pthread_stats_updater, NULL);
+   pthread_join(pthread_sig_handler, NULL);
+
+   stats_close();
+   ui_free(gui);
+
+   return 0;
+}
