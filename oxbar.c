3111887390b3c1eaae673a36bb71344096c0ea63
ryan <ryan.flannery@gmail.com>
2018-11-03 23:31:30 -0400

    More TODOs

--- oxbar.c
+++ oxbar.c
@@ -1,8 +1,9 @@
 /*
  * Various outstanding stuff that doesn't belong elsewhere...
  * TODO complete man page (probably wait until widget settings moved)
- * TODO don't bomb on more than one egress interface / make configurable
- * TODO don't use -1 for y/w/h magic - use 0 and use unsigned types
+ * TODO don't bomb on more than one egress interface / configurable (XXX MAN)
+ * TODO don't use -1 for y/w/h magic - use 0 and use unsigned types (XXX MAN)
+ * TODO fix font sizing issue (support pt/none/etc) (XXX MAN)
  * TODO more examples
  * TODO polish error messages (err/errx calls)
  * TODO make window FLOAT & ALWAYS-ON-TOP properties set-able
