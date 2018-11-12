bceea6be2d8ee3e8e8fced3535d9de8a01de7e13
Ryan Flannery <ryan.flannery@gmail.com>
2018-11-12 02:02:26 -0500

    Add LICENSE blurb to top of most files

--- gui/chart.h
+++ gui/chart.h
@@ -1,8 +1,24 @@
+/*
+ * Copyright (c) 2018 Ryan Flannery <ryan.flannery@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #ifndef CHART_H
 #define CHART_H
 
-#include <stdbool.h>
 #include <sys/types.h>
+#include <stdbool.h>
 
 /*
  * A chart is a set of N sampls of M distinct series, where all sampled values
